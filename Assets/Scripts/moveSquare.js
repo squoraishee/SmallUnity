dae4216c3b4ab7df36e6adadd7017ff97fecb1ac
Shafik.Quoraishee <Shafik.Quoraishee@viacommix.com>
2013-08-06 13:06:34 -0400

    Update

---
+++ Assets/Scripts/moveSquare.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+
+var g : GameObject;
+
+function Start () {
+
+}
+
+function Update () {
+		// Move the object forward along its z axis 1 unit/second.
+		transform.Translate(Vector3.forward * Time.deltaTime);
+		// Move the object upward in world space 1 unit/second.
+		transform.Translate(Vector3.up * Time.deltaTime, Space.World);
+}
